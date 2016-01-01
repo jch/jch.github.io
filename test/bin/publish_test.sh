0a91ef17d6c862c22296e91366aee3b004ceac04
Jerry Cheung <jollyjerry@gmail.com>
2016-01-01 15:13:58 -0800

    add publish test
    using BATS_TMPDIR because it's more reliable than having a test/fixtures
    directory since we rely on modified timestamps on files

---
+++ test/bin/publish_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bats
+
+function setup {
+  mkdir -p "$BATS_TMPDIR/src"
+  touch "$BATS_TMPDIR/src/old_file.md"
+  mkdir -p "$BATS_TMPDIR/posts"
+}
+
+@test "publish" {
+  touch "$BATS_TMPDIR/src/new_file.md"
+  bin/publish -s "$BATS_TMPDIR/src" -o "$BATS_TMPDIR/posts"
+  [[ -f "$BATS_TMPDIR/posts/new_file.html" ]]
+}
+
+@test "publish skips files older than output directory" {
+  bin/publish -s "$BATS_TMPDIR/src" -o "$BATS_TMPDIR/posts"
+  [[ ! -f "$BATS_TMPDIR/posts/file.html" ]]
+}
+
+@test "publish updates when file is modified" {
+  touch "$BATS_TMPDIR/src/old_file.md"
+  bin/publish -s "$BATS_TMPDIR/src" -o "$BATS_TMPDIR/posts"
+
+  [[ -f "$BATS_TMPDIR/posts/old_file.html" ]]
+}
